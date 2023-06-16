b8715b177ce6d1c976d91e6de1cf0b7dbfbd7e11
Prabhakar Mandal <prabhask856@gmail.com>
2023-06-16 17:48:32 +0530

    added variables and styles

--- src/components/ExpenseItems.jsx
+++ src/components/ExpenseItems.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./ExpenseItems.css";
 
 const ExpenseItems = () => {
-  return (
-    <div>
-      <h2> Expense Items </h2>
-      <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ul>
-    </div>
-  )
-}
+  const expenseDate = new Date(2023, 6, 16);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 310.50;
+  const locationOfExpenditure = 'India';
 
-export default ExpenseItems
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price"> {locationOfExpenditure}</div>
+        <div className="expense-item__price"> ${expenseAmount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItems;
