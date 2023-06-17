bdb5494b95486f06058e5ef7322b5de5a8ad3b43
Prabhakar Mandal <prabhask856@gmail.com>
2023-06-17 18:13:42 +0530

    add change expense button

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
-const ExpenseItem = ({date, title, amount, locationOfExpenditure, id}) => {
-  const deleteExpense = (e) => {
-    console.log('called');
-  }
+const ExpenseItem = ({ date, title, amount, locationOfExpenditure, id }) => {
+  const [expense, setExpense] = useState(amount);
+
+  const updateExpense = () => {
+    setExpense("100");
+  };
+
+  const deleteExpense = () => {
+    console.log("called");
+  };
 
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
-        <div className='expense-item__price'>{locationOfExpenditure}</div>
-        <button onClick={deleteExpense} className='expense-item__price' style={{backgroundColor:'darkred', borderRadius: '8px'}}>Delete</button>
+        <div className="expense-item__price">${expense}</div>
+        <button
+          onClick={updateExpense}
+          className="expense-item__price"
+          style={{ backgroundColor: "darkgreen", borderRadius: "8px" }}
+        >
+          Change Exp
+        </button>
+        <div className="expense-item__price">{locationOfExpenditure}</div>
+        <button
+          onClick={deleteExpense}
+          className="expense-item__price"
+          style={{ backgroundColor: "darkred", borderRadius: "8px" }}
+        >
+          Delete
+        </button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
